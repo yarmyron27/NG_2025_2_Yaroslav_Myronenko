1a7ba5c0418baadbe2dc704fb2863492ad5094fe
Yaroslav Myronenko <jarik2707@gmail.com>
2025-11-12 19:13:29 +0200

    fixed size array set up some validation and made the action selection from switch

--- Lesson_2/Task_1/main.cpp
+++ Lesson_2/Task_1/main.cpp
@@ -4,64 +4,83 @@ using namespace std;
 
 int main()
 {
-    const int num_account = 11;
+    const int num_account = 10;
     double account[num_account] = {0};
     int choise = 0;
+    int index = 0;
+    double amount = 0;
+    double sum = 0;
 
     do {
-        cout << "Menu Bank!" << endl;
-        cout << "1. Put money in the bank" << endl;
-        cout << "2. Withdraw money from the bank" << endl;
-        cout << "3. Show all bank accounts" << endl;
-        cout << "4. Amount in all bank accounts" << endl;
-        cout << "0. Exit" << endl;
-        cout << "Enter the action you want --> ";
-        cin >> choise;
+    cout << "Menu Bank!" << endl;
+    cout << "1. Put money in the bank" << endl;
+    cout << "2. Withdraw money from the bank" << endl;
+    cout << "3. Show all bank accounts" << endl;
+    cout << "4. Amount in all bank accounts" << endl;
+    cout << "0. Exit" << endl;
+    cout << "Enter the action you want --> ";
+    cin >> choise;
 
-        if (choise == 1) {
-            int index = 1;
-            double amount = 0;
+
+        switch (choise) {
+        case 1:
             cout << "\nEnter an account number from 1 to 10 --> ";
             cin >> index;
 
-            cout << "Enter the amount you want to deposit into the account --> " << endl;
-            cin >> amount;
-
-            account[index] += amount;
-            cout << "Accounts: " << index << " now has " << account[index] << "$" << endl;
-        }
-
-        else if (choise == 2) {
-            int index = 0;
-            double amount = 0;
-
-            cout << "\nEnter an account number from 1 to 10 --> ";
-            cin >> index;
-
-            cout << "Enter the amount to withdraw --> " << endl;
-            cin >> amount;
-
-            if (account[index] > amount) {
-                account[index] -= amount;
-                cout << "Funds successfully withdrawn " << "Account balance " << account[index] << " $" << endl;
+            if (index < 1 || index > 10) {
+                cout << "Error invalid input enter index from 1 to 10 --> ";
+                break;
             }
-            else cout << "Insufficient funds";
-        }
-        else if (choise == 3) {
+
+            cout << "Enter the amount you want to deposit into the account --> ";
+            cin >> amount;
+
+            account[index - 1] += amount;
+            cout << "Accounts: " << index << " now has " << account[index - 1] << "$" << endl;
+            break;
+
+        case 2:
+            cout << "\nEnter an account number from 1 to 10 --> ";
+            cin >> index;
+            if (index < 1 || index > 10) {
+                cout << "Enter corect account number! (1-10) --> ";
+                break;
+            }
+
+            cout << "Enter the amount to withdraw --> ";
+            cin >> amount;
+
+            if (account[index - 1] > amount) {
+                account[index - 1] -= amount;
+                cout << "Funds successfully withdrawn \nAccount balance " << account[index - 1] << " $" << endl;
+            }
+            else cout << "Insufficient funds" << endl;
+            break;
+
+        case 3:
             cout << "\nAll accounts: " << endl;
-            for (int index = 1; index < num_account; index++) {
-                cout << index << " score: " << account[index] << " $" << endl;
+            for (int index = 0; index < num_account; index++) {
+                cout << index + 1 << " score: " << account[index] << " $" << endl;
             }
-        }
-        else if (choise == 4) {
-            double sum = 0;
+            break;
 
-            for (int index = 1; index < num_account; index++) {
+        case 4:
+            for (int index = 0; index < num_account; index++) {
                 sum += account[index];
             }
             cout << "\nThe amount of money in all accounts " << sum << " $" << endl;
+            break;
+
+        case 0:
+            cout << "Exiting program..." << endl;
+            break;
+
+        default:
+            cout << "Error: Invalid operation." << endl;
+            return 0;
         }
-    }
-    while (choise != 0);
+    } while (choise != 0);
+
     return 0;
+
 }
